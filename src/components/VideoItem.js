8d616547f435f8ba6d02995d682b8ad789de401e
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-06-17 20:14:47 -0400

    setting up component videoItem and importing to VideoList

---
+++ src/components/VideoItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const VideoItem = (props) => {
+return (
+
+ )   
+}
+
+export default VideoItem
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import VideoItem from './VideoItem'
+
 
 const VideoList = (props) => {
 
 
 return (
     <div className=" ui relaxed divided list">
-    
+     
     </div>
 )
 }
+
+export default VideoList
